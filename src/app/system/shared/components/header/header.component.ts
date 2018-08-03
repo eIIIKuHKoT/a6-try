f7d06a9b6a7e872b8ce77100c50d12fc4d00eed1
Evgeny <evgeny.pochtarenko@softermii.com>
2018-08-03 14:45:37 +0300

    sidebar, header

---
+++ src/app/system/shared/components/header/header.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
+
+import {User} from "../../../../shared/models/user.model";
+import {AuthService} from "../../../../shared/services/auth.service";
+
+
+@Component({
+  selector: 'ek-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  data: Date = new Date();
+  user: User;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+
+    this.user = JSON.parse(window.localStorage.getItem('user'));
+
+  }
+
+  onLogOut() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+}
